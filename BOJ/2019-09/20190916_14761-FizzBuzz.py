2e152ed710be90e1b0f526dc1e4204c87c9d16f3
Inseo Park <wb092311@naver.com>
2019-09-17 17:23:43 +0900

    Update 20190916_14761-FizzBuzz.py

--- BOJ/2019-09/20190916_14761-FizzBuzz.py
+++ BOJ/2019-09/20190916_14761-FizzBuzz.py
@@ -1,11 +1,10 @@
 x, y, n = map(int, input().split())
+fb = {1:'Fizz', 2:'Buzz', 3:'FizzBuzz'}
 
 for i in range(1, n+1):
-    if not i%x and not i%y:
-        print('FizzBuzz')
-    elif not i%x:
-        print('Fizz')
-    elif not i%y:
-        print('Buzz')
-    else:
-        print(i)
+    flag = 0
+    if not i%x:
+        flag+=1
+    if not i%y:
+        flag+=2
+    print(fb.get(flag, i))
