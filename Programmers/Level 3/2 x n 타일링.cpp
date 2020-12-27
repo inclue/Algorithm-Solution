62c10e8336b05024bdf442281fa3e98663bbd492
Inseo Park <wb092311@naver.com>
2020-12-28 00:24:36 +0900

    Create 2 x n 타일링.cpp

---
+++ Programmers/Level 3/2 x n 타일링.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int solution(int n)
+{
+    vector<int> dp(n + 1, 0);
+    
+    //초기값
+    dp[0] = 1, dp[1] = 1;
+    
+    //점화식
+    for(int i = 2;i <= n;i++) dp[i] = (dp[i - 1] + dp[i - 2]) % 1000000007;
+    
+    //결과
+    return dp[n];
+}
