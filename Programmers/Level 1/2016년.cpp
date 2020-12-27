1905235a95aa0816adf711d46d6397e9a13ef179
Inseo Park <wb092311@naver.com>
2020-12-28 00:08:19 +0900

    Update 2016년.cpp

--- Programmers/Level 1/2016년.cpp
+++ Programmers/Level 1/2016년.cpp
@@ -1,7 +1,8 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-string solution(int a, int b) {
+string solution(int a, int b)
+{
     int month[13] = {0, 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
     string res[7] = {"SUN", "MON", "TUE", "WED", "THU", "FRI", "SAT"};
     
