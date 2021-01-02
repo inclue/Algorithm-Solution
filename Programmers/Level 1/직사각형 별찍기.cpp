bce8659975024deb388faf7987a1662aa0e9a438
Inseo Park <wb092311@naver.com>
2021-01-02 15:45:05 +0900

    Create 직사각형 별찍기.cpp

---
+++ Programmers/Level 1/직사각형 별찍기.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(void)
+{
+    int n,m;
+    scanf("%d %d", &n, &m);
+    for(int i = 0;i < m;i++)
+    {
+        for(int j = 0;j < n;j++) printf("*");
+        printf("\n");
+    }
+    return 0;
+}
