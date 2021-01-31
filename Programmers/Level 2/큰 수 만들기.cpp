41232d36063e5ec2b70825e772d73332e24fd694
Inseo Park <wb092311@naver.com>
2021-01-31 23:08:10 +0900

    Create 큰 수 만들기.cpp

---
+++ Programmers/Level 2/큰 수 만들기.cpp
@@ -0,0 +1,27 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+string solution(string number, int k) {
+    string answer = "";
+    int idx = 0;
+    
+    //for문 돌면서 숫자 1개씩 선택해주기
+    for(int i = 0;i < number.size() - k;i++)
+    {
+        //가장 큰 숫자 1개 선택
+        int maxidx = idx;
+        for(int j = idx;j <= i + k;j++)
+        {
+            if(number[maxidx] < number[j]) maxidx = j;
+        }
+        
+        //정답에 추가
+        idx = maxidx + 1;
+        answer.push_back(number[maxidx]);
+    }
+    
+    
+    return answer;
+}
